a7164042af281e884b6656d0cc3f3488628f82d0
Mariajosé Coronado Del Castillo <66452068+mariajosecoronado@users.noreply.github.com>
2020-11-24 04:06:53 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const routes = require('./routers/routers');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({origin: '*'})); 
+//app.use(cors({origin: 'https://kuepj-3000.sse.codesandbox.io'}));
+
+// // Routes//
+// app.use('/api',users);
+// app.use('/api',register);
+// app.use('/api',routes);
+
+let port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log("JSON Server ejecutandose");
+});
